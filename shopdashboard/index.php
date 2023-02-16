afc799969308d57d1af8bfa49a6db56a18110c84
olvrvitoria <112016371+olvrvitoria@users.noreply.github.com>
2023-02-16 10:39:30 -0300

    shopdashboard php

---
+++ shopdashboard/index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+ob_start();
+
+include_once '../includes/config.php';
+?>
+
+<!-------------------
+  SEÇÃO HTML ABERTA
+-------------------->
+
+<!doctype html>
+<html lang="pt-br">
+ 
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+      <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+      <link rel="stylesheet" href="assets/libs/css/style.css">
+      <link rel="stylesheet" href="assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+      <link rel="stylesheet" href="assets/vendor/charts/chartist-bundle/chartist.css">
+      <link rel="stylesheet" href="assets/vendor/charts/morris-bundle/morris.css">
+      <link rel="stylesheet" href="assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
+      <link rel="stylesheet" href="assets/vendor/charts/c3charts/c3.css">
+      <link rel="stylesheet" href="assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
+      <title>Dashboard Lojista - Venus Shop</title>
+  </head>
+
+  <body>
+
+    <header>
+      <div class="dashboard-main-wrapper">
+        <!-- Menu -->
+        <div class="dashboard-header">
+          <nav class="navbar navbar-expand-lg bg-white fixed-top">
+            <a class="navbar-brand" href="index.php">título aqui</a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+      
+    </header>
+
+  </body>
+
+
+
+</html>
+
+<!----------------------
+  SEÇÃO HTML ENCERRADA
+----------------------->
